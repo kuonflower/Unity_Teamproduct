5b9681e7dd696bc889118aa61ae1f3df56938ecd
kuonflower <kuonflower@gmail.com>
2020-06-10 00:08:40 +0900

    敵オブジェクト制作

---
+++ TeamProduct20191216AM/Assets/Tanigawa/Scripts/CollisionDetector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+
+[RequireComponent(typeof(Collider))]
+public class CollisionDetector : MonoBehaviour
+{
+
+    [SerializeField] private TriggerEvent onTriggerStay = new TriggerEvent();
+
+    //Is Trigger がONで他のColliderと重なっているときはこのメソッドが常にコールされる
+
+    //   <param name="other"></param>
+    private void OnTriggerStay(Collider other)
+    {
+        onTriggerStay.Invoke(other);
+    }
+
+    [Serializable]
+    public class TriggerEvent : UnityEvent<Collider>
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TeamProduct20191216AM/Assets/Tanigawa/Scripts/EnemyMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMove : MonoBehaviour
+{
+
+  //  [SerializeField] private PlayerController playerController;
+
+    private NavMeshAgent _agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void OnDetectObject(Collider collider)
+    {
+        //検知したオブジェクトに「Player」tagが付いてたらそのオブジェクトを追いかける
+        if (collider.CompareTag("Player"))
+        {
+            _agent.destination = collider.transform.position;
+        }
+    }
+
+   /* // Update is called once per frame
+    void Update()
+    {
+        _agent.destination = playerController.transform.position;
+    }*/
+}
