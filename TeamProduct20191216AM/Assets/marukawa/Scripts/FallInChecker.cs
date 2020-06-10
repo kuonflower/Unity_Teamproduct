8144b5552137e76da278029d2f4a29dbffcbbcdb
marufumi <marufumi39@gmail.com>
2020-06-10 15:44:39 +0900

    ゲームタイトル画面とMain画面のシーンの切り替えとオブジェクトを作りました

---
+++ TeamProduct20191216AM/Assets/marukawa/Scripts/FallInChecker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallInChecker : MonoBehaviour
+{
+    public Hole red;
+    // Start is called before the first frame update
+    void OnGUI()
+     {
+        string label = " ";
+
+        if(red.IsGORL())
+        {
+            label = "Fall in hole!";
+        }
+        GUI.Label(new Rect(0, 0, 100, 30), label);
+     }
+
+
+
+    /*void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+}
