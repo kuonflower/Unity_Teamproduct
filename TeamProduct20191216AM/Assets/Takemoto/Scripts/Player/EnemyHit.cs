c4a294c129212f9bee96e38b1d91d6cab90e9d21
DESKTOP-4KLFA8B\191216AM <daiki.0217kei@gmail.com>
2020-06-11 14:48:55 +0900

    2020,06,11 14時48分帰宅前作業保存　竹本

---
+++ TeamProduct20191216AM/Assets/Takemoto/Scripts/Player/EnemyHit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour
+{
+    [SerializeField]
+    int attackPower;
+
+    Status enemyStatus;         // 敵のステータス
+    private GameObject enemy;   // 敵となるオブジェクト
+
+    //オブジェクトと接触した瞬間に呼び出される
+    void OnTriggerEnter(Collider other)
+    {
+        
+   
+        //攻撃した相手がEnemyの場合
+        if (other.CompareTag("Enemy"))
+        {
+
+            enemyStatus.Damage(attackPower);
+            Debug.Log("HPだよ");
+
+        }
+    }
+}
