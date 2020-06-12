e88541882d588b818a8825ed9986bea5f83a875b
marufumi <marufumi39@gmail.com>
2020-06-12 15:41:29 +0900

    コインの数をUIで表示することができました

---
+++ TeamProduct20191216AM/Assets/marukawa/Scripts/CoinCount.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCount : MonoBehaviour
+{
+    //public static int coinCount;
+    // Start is called before the first frame update
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+
+        string label = "coinの枚数:" + CoinDestroyer.coinCount;
+
+        GUI.Label(new Rect(0, 0, 100, 30), label);
+    }
+
+    /*void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+}
