e832a7e2316da84c98fdd390c03edd928047b32e
kuonflower <kuonflower@gmail.com>
2020-06-14 15:25:05 +0900

    ユニティーちゃんと敵のHPとダメージのやり取りのスクリプト完成

---
+++ TeamProduct20191216AM/Assets/Tanigawa/Scripts/PlayerAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField]
+    public int attackPower = 1;
+
+    //EnemyStatus enemyStatus;         // 敵のステータス
+    private GameObject enemy;   // 敵となるオブジェクト
+
+    //オブジェクトと接触した瞬間に呼び出される
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        //攻撃した相手がEnemyの場合
+        if (other.CompareTag("Enemy"))
+        {
+            var targetEnemyStatus = other.GetComponent<EnemyStatus>();
+            targetEnemyStatus.Damage(attackPower);
+            Debug.Log("HPだよ");
+
+        }
+    }
+}
