69eaf7da64f0488b595ec20892d47284aaa0794d
tahara_ryuuji <maruboro0421@yahoo.co.jp>
2020-06-16 09:14:38 +0900

    エレベータースクリプトの変更

--- TeamProduct20191216AM/Assets/tahara/script/EnemyAreaLockIn.cs
+++ TeamProduct20191216AM/Assets/tahara/script/EnemyAreaLockIn.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class EnemyAreaLockIn : MonoBehaviour
 {
     public GameObject Elevator;
-    public string Enemy;
+    //public string Enemy;
+    public Collider other;
 
     void Start()
     {
@@ -15,27 +16,45 @@ public class EnemyAreaLockIn : MonoBehaviour
 
     void Update()
     {
-
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if(other.gameObject.tag == "Enemy")
+        Debug.Log("Updateの中身 this.other :" + this.other  );
+        if (this.other == null)
         {
             Elevater.flag = true;
+            Debug.Log("null" + Elevater.flag);
+            //GameObject Enemy = GameObject.FindGameObjectWithTag("Enemy");
+            //if (Enemy == null)
+            //{
+                
+            //}
         }
-
-        Debug.Log("Exit" + Elevater.flag);
     }
 
-    void OnTriggeEnter(Collider other)
-    {
-       
-
-        Debug.Log("Enter" + Elevater.flag);
-    }
     void OnTriggerStay(Collider other)
     {
-        Debug.Log("Stay" + Elevater.flag);
+        if (other.gameObject.tag == "Enemy")
+        {
+            this.other = other;
+            Debug.Log("オントリガーステイの中身 this.other :" + this.other + "other :" + other);
+            Debug.Log("オントリガーステイ　other" + Elevater.flag);
+        }
+    
     }
 }
+
+
+//void OnTriggerExit(Collider other)
+//{
+//    if(other.gameObject.tag == "Enemy")
+//    {
+//        Elevater.flag = true;
+//    }
+
+//    Debug.Log("Exit" + Elevater.flag);
+//}
+
+//void OnTriggeEnter(Collider other)
+//{
+
+
+//    Debug.Log("Enter" + Elevater.flag);
+//}
