435a6afea4882179087a033616fef817e5bd7483
tahara_ryuuji <maruboro0421@yahoo.co.jp>
2020-06-16 15:08:42 +0900

    ゲームオーバーゾーンの追加

---
+++ TeamProduct20191216AM/Assets/tahara/script/GameOverZone.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverZone : MonoBehaviour
+{
+    public bool GameOverflag = false;
+    public GameObject showObject;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    
+    void onTriggerEnter(Collider gameover)
+    {
+        if(gameover.gameObject.tag == "Player")
+        {
+            GameOverflag = true;
+        }
+    }
+
+    void OnGUI()
+    {
+        string label = " ";
+
+        if (GameOverflag)
+        {
+            label = "GameOver";
+            showObject.SetActive(true);
+
+
+        }
+        GUI.Label(new Rect(300, 80, 500, 300), label);
+    }
+
+}
